61f193aebcc100063bc12011933febb33421cb76
Ana Ribeiro <analidialirar@gmail.com>
2022-07-30 00:02:10 -0300

    bugfix: add get /

--- src/app.js
+++ src/app.js
@@ -4,19 +4,19 @@ const cors = require('cors')
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger/swagger_output.json');
 const mongoose = require('./database/mongooseConnect')
+const index = require('../src/routes/index.js');
 const matchesRoutes = require('./routes/matchesRoutes')
 const userRoutes = require('./routes/userRoutes')
 
 const app = express()
-
 app.use(express.json())
 app.use(cors())
-app.use('/mariachuteira-documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-mongoose.connect()
 
+app.use('/mariachuteira-documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/', index);
 app.use(matchesRoutes)
 app.use(userRoutes)
 
-
+mongoose.connect()
 
 module.exports = app
