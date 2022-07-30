22b9bafeab0574baba56f492f4ae4401b8d67763
Ana Ribeiro <analidialirar@gmail.com>
2022-07-29 23:16:51 -0300

    deploy: swagger and heroku

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require('dotenv-safe').config();
 const express = require('express')
 const cors = require('cors')
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger/swagger_output.json');
 const mongoose = require('./database/mongooseConnect')
 const matchesRoutes = require('./routes/matchesRoutes')
 const userRoutes = require('./routes/userRoutes')
@@ -9,9 +11,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 mongoose.connect()
 
 app.use(matchesRoutes)
 app.use(userRoutes)
+
+
 
 module.exports = app
