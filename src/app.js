173650b1092cb86a90c21ea80f8473de9e6dfc2b
Ana Ribeiro <analidialirar@gmail.com>
2022-07-30 08:50:59 -0300

    bugfix: deploy

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 require('dotenv-safe').config();
 const express = require('express')
-const cors = require('cors')
+//const cors = require('cors')
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger/swagger_output.json');
 const mongoose = require('./database/mongooseConnect')
@@ -10,7 +10,14 @@ const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+});
 
 app.use('/mariachuteira-documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/', index);
